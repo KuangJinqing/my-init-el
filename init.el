c382d891f96f32cced86bcaa668c35c4b7995889
kuangjinqing <kuangjinqingcn@gmail.com>
2020-03-21 23:56:50 +0800

    Small ajustments

--- init.el
+++ init.el
@@ -6,8 +6,6 @@
 
 (fset 'yes-or-no-p 'y-or-n-p)
 (global-set-key [remap list-buffers] 'ibuffer)
-(add-hook 'c-mode-hook 'linum-mode)
-(add-hook 'c++-mode-hook 'linum-mode)
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
@@ -61,20 +59,25 @@
 (use-package company
   :ensure t
   :config
-  (global-company-mode)
+  (global-company-mode t)
+  (add-hook 'eshell-mode-hook (lambda()(company-mode -1)))
   (define-key company-active-map (kbd "M-n") nil)
   (define-key company-active-map (kbd "M-p") nil)
   (define-key company-active-map (kbd "C-n") 'company-select-next)
   (define-key company-active-map (kbd "C-p") 'company-select-previous))
 
+(use-package treemacs
+  :ensure t
+  :config (define-key global-map (kbd "M-0") 'treemacs-select-window))
+
 (use-package lsp-mode
   :ensure t
-  :init
+  :config
   (add-hook 'c-mode-hook 'lsp)
   (add-hook 'c++-mode-hook 'lsp)
   (add-hook 'python-mode 'lsp)
-  :commands lsp
-  :config )
+  (add-hook 'lsp-mode-hook 'linum-mode)
+  (define-key lsp-mode-map (kbd "C-c l") lsp-command-map))
 
 (use-package lsp-ui
   :ensure t
@@ -82,19 +85,16 @@
 
 (use-package company-lsp
   :ensure t
-  :commands company-lsp
+  :requires company
   :config
   (push 'company-lsp company-backends))
 
-(use-package treemacs
-  :ensure t
-  :config (define-key global-map (kbd"M-0") 'treemacs-select-window))
-
 (use-package lsp-treemacs
   :ensure t
-  :commands lsp
   :config
-  (lsp-treemacs-sync-mode t))
+  (lsp-treemacs-sync-mode t)
+  (define-key lsp-mode-map (kbd "M-1") 'lsp-treemacs-symbols)
+  (define-key lsp-mode-map (kbd "M-2") 'lsp-treemacs-errors-list))
 
 (use-package projectile
   :ensure t
@@ -112,13 +112,6 @@
 (use-package treemacs-projectile
   :after treemacs projectile
   :ensure t)
-
-(use-package hl-todo
-  :ensure t
-  :config
-  (global-hl-todo-mode t)
-  (define-key hl-todo-mode-map (kbd "C-c o") 'hl-todo-occur)
-  (define-key hl-todo-mode-map (kbd "C-c i") 'hl-todo-insert))
 
 (use-package yasnippet
   :ensure t
@@ -138,27 +131,32 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(auto-insert-directory "~/Templates/")
  '(auto-save-default nil)
  '(before-save-hook (quote (delete-trailing-whitespace lsp-format-buffer)))
  '(column-number-mode t)
- '(company-idle-delay 0.1)
+ '(company-idle-delay 0)
+ '(company-lsp-enable-snippet nil)
+ '(company-minimum-prefix-length 1)
  '(delete-selection-mode t)
  '(electric-indent-mode t)
  '(electric-pair-mode t)
  '(flycheck-disabled-checkers (quote (emacs-lisp-checkdoc javascript-jshint)))
+ '(global-auto-revert-mode t)
  '(indent-tabs-mode nil)
  '(inhibit-startup-screen t)
- '(lsp-clients-clangd-args (quote ("-header-insertion=never")))
+ '(lsp-clients-clangd-args
+   (quote
+    ("-j=6" "-background-index" "-log=error" "-header-insertion=never")))
  '(lsp-diagnostic-package :flycheck)
- '(lsp-keymap-prefix "C-c l")
+ '(lsp-enable-snippet nil)
  '(make-backup-files nil)
  '(menu-bar-mode nil)
  '(package-check-signature nil)
  '(package-enable-at-startup nil)
  '(package-selected-packages
    (quote
-    (counsel-projectile exec-path-from-shell flx yasnippet which-key treemacs-projectile material-theme hl-todo projectile lsp-treemacs company-lsp lsp-ui lsp-mode company flycheck expand-region undo-tree counsel ivy ace-window use-package)))
+    (lsp-treemacs company-lsp lsp-ui lsp-mode counsel-projectile exec-path-from-shell flx yasnippet which-key treemacs-projectile material-theme projectile company flycheck expand-region undo-tree counsel ivy ace-window use-package)))
+ '(projectile-track-known-projects-automatically nil)
  '(show-paren-mode t)
  '(tool-bar-mode nil)
  '(treemacs-filewatch-mode t)
@@ -170,4 +168,3 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(put 'scroll-left 'disabled nil)
